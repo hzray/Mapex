888ecf1af89c23d7b5f757cb31a06d381cb58520
ziruih <ziruih73@gmail.com>
2020-11-28 12:35:26 -0800

    add new html

--- DianPing/application.py
+++ DianPing/application.py
@@ -44,6 +44,12 @@ def makeDataFrame(reviewArr):
     return df
 
 
+@app.route("/random")
+def random():
+    return render_template('report.html')
+
+
+
 if __name__ == "__main__":
     app.debug = True
-    app.run('0.0.0.0') 
+    app.run(host='0.0.0.0') 
